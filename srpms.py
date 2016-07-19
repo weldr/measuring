d44e1df239d606fc2e5e7d5c84021fcdfb257c5b
Chris Lumens <clumens@redhat.com>
2016-07-19 16:41:17 -0400

    Also output the number and names of packages built from an SRPM.
    By default the results are sorted by SRPM name.  If you'd like to see it
    in terms of number of subpackages, do this:
    
    $ ./srpms.py | sort --key=3 -n -r | less

--- srpms.py
+++ srpms.py
@@ -13,6 +13,13 @@ base.fill_sack()
 
 q = base.sack.query().available()
 
-pkgs = {pkg.sourcerpm for pkg in list(q)}
-for p in sorted(list(pkgs)):
-    print(p)
+srpms = {}
+
+for pkg in list(q):
+    if pkg.sourcerpm in srpms:
+        srpms[pkg.sourcerpm].append(pkg.name)
+    else:
+        srpms[pkg.sourcerpm] = [pkg.name]
+
+for (key, val) in sorted(srpms.items()):
+    print("%s = %s %s" % (key, len(val), val))

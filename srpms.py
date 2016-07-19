404c005f62a7e96403a065292318e254f2bed182
Chris Lumens <clumens@redhat.com>
2016-07-19 16:27:56 -0400

    Add a program that lists SRPMs, sorted.
    You could pipe this output into wc if you want to know how many SRPMs
    there are.  I suppose you could do other things with it too.  It gets
    rid of duplicates.  Maybe a useful thing to do would be also output how
    many RPMs are built from a given SRPM.

---
+++ srpms.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+#
+# Print a sorted list of all SRPMs.
+# This works against the repos configured on the system.  So if you've got
+# RHEL7 installed, it will tell you about RHEL7.  If you've got rawhide, it'll
+# tell you about that.
+
+import dnf
+
+base = dnf.base.Base()
+base.read_all_repos()
+base.fill_sack()
+
+q = base.sack.query().available()
+
+pkgs = {pkg.sourcerpm for pkg in list(q)}
+for p in sorted(list(pkgs)):
+    print(p)

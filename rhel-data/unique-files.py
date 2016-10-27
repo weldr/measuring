943eff79694d5996a9781a782994e77bb235b8e5
Will Woods <wwoods@redhat.com>
2016-10-27 12:22:03 -0400

    remove hardcoded filename

--- rhel-data/unique-files.py
+++ rhel-data/unique-files.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python
 
-def main():
-    sums = [line.strip().split(None,1) for line in open("SHA256SUM.rpms")]
+import sys
+
+def main(sumfile):
+    sums = [line.strip().split(None,1) for line in open(sumfile)]
     uniq_sums = dict(sums)
     for shasum, filename in uniq_sums.items():
         print("{}  {}".format(shasum,filename))
 
 if __name__ == '__main__':
     try:
-        main()
+        main(sys.argv[1])
     except IOError as e:
         print(str(e))
         raise SystemExit(1)
